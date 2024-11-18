eb580d5225f2f46188219c4e109231932522ea80
noel <noelnolife@gmail.com>
2024-11-18 21:07:22 +0700

    markdown previwer

--- src/Pages/MarkdownPreviewer.js
+++ src/Pages/MarkdownPreviewer.js
@@ -1,8 +1,56 @@
-import React from 'react'
+import { marked  } from 'marked'
+import React, {useState, useEffect} from 'react'
+
+export const DEFAULT_VALUE = `# React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`javascript
+// this is multi-line code:
+
+function sneed(feed, seed) {
+  if (feed == '\`\`\`' && seed == '\`\`\`') {
+    return sneed;
+  }
+}
+\`\`\`
+**bold** Text
+_italic_ Text.
+**_both!_**
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+1. Number 1
+1. Number 2
+1. Number 3
+
+![Sneed Logo](https://i.ytimg.com/vi/0O2NV1enG-k/maxresdefault.jpg)
+`
 
 function MarkdownPreviewer() {
+  const [markdownInput, setMarkdownInput] = useState(DEFAULT_VALUE)
+  const [markdownOutput, setMarkdownOutput] = useState("")
+
+  useEffect(() => {
+    markdownParser(markdownInput)
+  }, [])
+  
+
+  const markdownParser = (input) => {
+    setMarkdownInput(input) 
+    setMarkdownOutput(marked(input, {breaks:true}))
+  }
+
   return (
-    <div>MarkdownPreviewer</div>
+    <div>
+      <textarea name="markdownInput" id="editor" onChange={e => markdownParser(e.target.value)} >{markdownInput}</textarea>
+      <div id='preview' dangerouslySetInnerHTML={{__html: marked.parse(markdownOutput, {breaks:true})}}></div>
+
+    </div>
   )
 }
 
