baf92e485b474e7cc0bde7e1a1d4d6268f42d8a2
noel <noelnolife@gmail.com>
2024-11-18 19:51:22 +0700

    adding router dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RandomQuoteMachine from './Pages/RandomQuoteMachine';
+import JavascriptCalculator from './Pages/JavascriptCalculator';
+import DrumMachine from './Pages/DrumMachine';
+import MarkdownPreviewer from './Pages/MarkdownPreviewer';
+import TwoFiveClock from './Pages/TwoFiveClock';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const router = createBrowserRouter([
+    {
+        path : "/",
+        element : <App/>,
+    },
+    {
+        path : "/random-quote-machine",
+        element : <RandomQuoteMachine/>
+    },
+    {
+        path: "/javascript-calculator",
+        element : <JavascriptCalculator/>
+    },
+    {
+        path : "/drum-machine",
+        element : <DrumMachine/>
+    },
+    {
+        path : "/markdown-previwer",
+        element : <MarkdownPreviewer/>
+    },
+    {
+        path : "/two-five-clock",
+        element : <TwoFiveClock/>
+    }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router}/>
+)
