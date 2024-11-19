90103431fffc2afb4c77dbc1e7f07f547332a414
noel <noelnolife@gmail.com>
2024-11-19 14:19:19 +0700

    adding style

--- src/Pages/DrumMachine.js
+++ src/Pages/DrumMachine.js
@@ -1,31 +1,32 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const DISPLAY_CHART = {
-	'q' : "Heater 1",
-	'w' : "Heater 2",
-	'e' : "Heater 3",
-	'a' : "Heater 4",
-	's' : "Clap",
-	'd' : "Open-HH",
-	'z' : "Kick-n'-Hat",
-	'x' : "Kick",
-	'c' : "Closed-HH"
+	'q': "Heater 1",
+	'w': "Heater 2",
+	'e': "Heater 3",
+	'a': "Heater 4",
+	's': "Clap",
+	'd': "Open-HH",
+	'z': "Kick-n'-Hat",
+	'x': "Kick",
+	'c': "Closed-HH"
 }
 
 function DrumMachine() {
 	const [displayString, setDisplayString] = useState("")
+	const [pressedKey, setPressedKey] = useState("")
 	useEffect(() => {
-	  document.addEventListener("keypress", handlePlay)
-	
-	  return () => {
-		document.removeEventListener("keypress", handlePlay)
-	  }
+		document.addEventListener("keypress", handlePlay)
+
+		return () => {
+			document.removeEventListener("keypress", handlePlay)
+		}
 	}, [])
 
 	function handlePlay(e) {
 		const audio = document.getElementById(e.key.toUpperCase())
 		const display = DISPLAY_CHART[e.key.toLowerCase()]
-		if(audio && display) {
+		if (audio && display) {
 			audio.play()
 			setDisplayString(display)
 		}
@@ -34,54 +35,78 @@ function DrumMachine() {
 	function buttonClick(val, elemId) {
 		const audio = document.getElementById(val)
 		const display = DISPLAY_CHART[elemId]
-		if(audio) {
+		if (audio) {
 			audio.play()
 			setDisplayString(display)
 			
 		}
 	}
-	
+
 	return (
 		<div id='drum-machine' className='container container-fluid'>
 			<div id="display">
 				<p>{displayString}</p>
 			</div>
-			<button className="drum-pad" id='q' onClick={e => buttonClick(e.target.value, e.target.id)} value={"Q"}>
-				Q
-				<audio className="clip" id="Q" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3"></audio>
-			</button>
-			<button className="drum-pad" id='w' onClick={e => buttonClick(e.target.value, e.target.id)} value={"W"}>
-				W
-				<audio className="clip" id="W" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3"></audio>
-			</button>
-			<button className="drum-pad" id='e' onClick={e => buttonClick(e.target.value, e.target.id)} value={"E"}>
-				E
-				<audio className="clip" id="E" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3"></audio>
-			</button>
-			<button className="drum-pad" id='a' onClick={e => buttonClick(e.target.value, e.target.id)} value={"A"}>
-				A
-				<audio className="clip" id="A" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3"></audio>
-			</button>
-			<button className="drum-pad" id='s' onClick={e => buttonClick(e.target.value, e.target.id)} value={"S"}>
-				S
-				<audio className="clip" id="S" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3"></audio>
-			</button>
-			<button className="drum-pad" id='d' onClick={e => buttonClick(e.target.value, e.target.id)} value={"D"}>
-				D
-				<audio className="clip" id="D" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3"></audio>
-			</button>
-			<button className="drum-pad" id='z' onClick={e => buttonClick(e.target.value, e.target.id)} value={"Z"}>
-				Z
-				<audio className="clip" id="Z" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3"></audio>
-			</button>
-			<button className="drum-pad" id='x' onClick={e => buttonClick(e.target.value, e.target.id)} value={"X"}>
-				X
-				<audio className="clip" id="X" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3"></audio>
-			</button>
-			<button className="drum-pad" id='c' onClick={e => buttonClick(e.target.value, e.target.id)} value={"C"}>
-				C
-				<audio className="clip" id="C" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3"></audio>
-			</button>
+			<div className="row">
+				<div className="col-sm-4">
+					<button className="drum-pad btn btn-outline-primary" id='q' onClick={e => buttonClick(e.target.value, e.target.id)} value={"Q"}>
+						Q
+						<audio className="clip" id="Q" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3"></audio>
+					</button>
+				</div>
+				<div className="col-sm-4">
+					<button className="drum-pad btn btn-outline-primary" id='w' onClick={e => buttonClick(e.target.value, e.target.id)} value={"W"}>
+						W
+						<audio className="clip" id="W" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3"></audio>
+					</button>
+				</div>
+				<div className="col-sm-4">
+					<button className="drum-pad btn btn-outline-primary" id='e' onClick={e => buttonClick(e.target.value, e.target.id)} value={"E"}>
+						E
+						<audio className="clip" id="E" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3"></audio>
+					</button>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-sm-4">
+					<button className="drum-pad btn btn-outline-primary" id='a' onClick={e => buttonClick(e.target.value, e.target.id)} value={"A"}>
+						A
+						<audio className="clip" id="A" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3"></audio>
+					</button>
+				</div>
+				<div className="col-sm-4">
+					<button className="drum-pad btn btn-outline-primary" id='s' onClick={e => buttonClick(e.target.value, e.target.id)} value={"S"}>
+						S
+						<audio className="clip" id="S" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3"></audio>
+					</button>
+				</div>
+				<div className="col-sm-4">
+					<button className="drum-pad btn btn-outline-primary" id='d' onClick={e => buttonClick(e.target.value, e.target.id)} value={"D"}>
+						D
+						<audio className="clip" id="D" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3"></audio>
+					</button>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-sm-4">
+					<button className="drum-pad btn btn-outline-primary" id='z' onClick={e => buttonClick(e.target.value, e.target.id)} value={"Z"}>
+						Z
+						<audio className="clip" id="Z" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3"></audio>
+					</button>
+				</div>
+				<div className="col-sm-4">
+					<button className="drum-pad btn btn-outline-primary" id='x' onClick={e => buttonClick(e.target.value, e.target.id)} value={"X"}>
+						X
+						<audio className="clip" id="X" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3"></audio>
+					</button>
+				</div>
+				<div className="col-sm-4">
+					<button className="drum-pad btn btn-outline-primary" id='c' onClick={e => buttonClick(e.target.value, e.target.id)} value={"C"}>
+						C
+						<audio className="clip" id="C" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3"></audio>
+					</button>
+				</div>
+			</div>
 		</div>
 	)
 }
